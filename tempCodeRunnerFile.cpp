0adce8db1ea76c96aab1d9b56335998bf66f6008
Lubna2597 <k250926@nu.edu.pk>
2026-04-21 10:53:25 +0500

    hi

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<fstream>
+#include<unistd.h>
+#include"friend.h"
+#include"login.h"
+#include"signUp.h"
+#include"todo.h"
+#include"note.h"
+using namespace std;
+int main(){
+    signUp SIGNUP;
+    login LOGIN;
+    string n;
+    int choose;
+    cout<<"Enter 0 to login and 1 to signup: ";
+    cin>>choose;
+   switch(choose){
+case 1:
+SIGNUP.checkU();
+case 2:
+LOGIN.loginCheck();
+cout<<"\nLoading ";
+for(int i=0;i<6;i++){
+    cout<<".";
+    sleep(1);
+
+}
+cout<<endl;
+   }
+
+}
